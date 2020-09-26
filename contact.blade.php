91acc2372ff785a7397d53d680a27b9314c302fb
binoy-saha01 <68859385+binoy-saha01@users.noreply.github.com>
2020-09-27 02:09:47 +0600

    Add files via upload

---
+++ contact.blade.php
@@ -0,0 +1,25 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+  <div class='container margin-top-20'>
+    <h2>Contact Us</h2>
+    <hr>
+
+    <div class="row">
+    	<div class="col-md-3">
+    		<h3>
+    			Basic Need
+    		</h3>
+    		<p>
+    			<address>
+    				Bashundhara, Dhaka.
+    				<br>
+    				Bangladesh
+    			</address>
+    		</p>
+    	</div>
+    	
+    </div>
+
+  </div>
+@endsection
