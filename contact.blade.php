49a444579617946828a615caff7dbdf7c326a3b3
Naeem1721 <48604038+Naeem1721@users.noreply.github.com>
2020-09-27 02:09:45 +0600

    Add files via upload

---
+++ contact.blade.php
@@ -0,0 +1,28 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+  <div class='container margin-top-20'>
+    <h2>Contact Us</h2>
+    <hr>
+
+    <div class="row">
+    	<div class="col-md-3">
+    		<h3>
+    			BasicNeed
+    		</h3>
+    		<p>
+    			<address>
+    				Dhaka,
+    				<br>
+    				Bangladesh
+    			</address>
+    		</p>
+    	</div>
+    	<div class="col-md-9">
+    		<h5>About Our Business</h5>
+    		
+    	</div>
+    </div>
+    
+  </div>
+@endsection
